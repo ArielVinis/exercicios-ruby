ce7a8e87f60998e00380034fc50a56716a3ff85e
Ariel França <ariel.franca@hotmail.com>
2025-05-15 11:13:41 -0300

    Add methods to count vowels and consonants

--- 2_count-vowels-consonants/lib/word.rb
+++ 2_count-vowels-consonants/lib/word.rb
@@ -1,5 +1,12 @@
 class Word
+  VOGAIS = "aeiouáéíóúàèìòùãõâêîôûäëïöüy"
+  CONSOANTES = "bcdfghjklmnpqrstvwxz"
+
   def vowels_count(phrase)
-    
+    phrase.downcase.count(VOGAIS)
+  end
+
+  def consonants_count(phrase)
+    phrase.downcase.count(CONSOANTES)
   end
 end
