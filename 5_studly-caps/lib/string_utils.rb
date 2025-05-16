379a77a259094fc2e86847e333cb83310466bb05
Ariel França <ariel.franca@hotmail.com>
2025-05-16 11:01:35 -0300

    Completed method to case transformation

--- 5_studly-caps/lib/string_utils.rb
+++ 5_studly-caps/lib/string_utils.rb
@@ -1,3 +1,13 @@
 class StringUtils
   def wavefy(string)
-  
+    string.chars.each_with_index.map do |char, index|
+      index.even? ? char.downcase : char.upcase
+    end.join
+  end
+end
+
+# string.chars → quebra a string em um array de letras: ["R", "u", "b", "y"]
+# .each_with_index → permite saber o índice de cada letra.
+# .upcase → transforma a letra em maiúscula.
+# .downcase → transforma a letra em minúscula.
+# .even? → verifica se o índice é par.
