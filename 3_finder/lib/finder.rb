0aee853a28c29628f3de09f349fb95bf8e9b13ff
Ariel França <ariel.franca@hotmail.com>
2025-05-15 23:37:03 -0300

    Add Finder class completed

--- 3_finder/lib/finder.rb
+++ 3_finder/lib/finder.rb
@@ -1,5 +1,11 @@
 class Finder
   def index_of(list, value)
-    
+    # list.each_index - Itera sobre os índices da lista (0, 1, 2, etc)
+    # select - Filtra apenas os índices que satisfazem a condição
+    # { |i| } - Bloco que define a condição para cada índice i
+    # list[i] - Obtém o elemento da lista na posição i
+    # .include?(value) - Verifica se o elemento contém o valor procurado
+    # .include? - Permite ler parte da string 
+    list.each_index.select { |i| list[i].include?(value) }
   end
 end
