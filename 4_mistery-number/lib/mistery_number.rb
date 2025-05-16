2fdbdd8db54a2d78c922ab68629ae894c5052722
Ariel França <ariel.franca@hotmail.com>
2025-05-16 00:40:39 -0300

    Completed: logic to calculate mystery number based on given operation

--- 4_mistery-number/lib/mistery_number.rb
+++ 4_mistery-number/lib/mistery_number.rb
@@ -1,5 +1,24 @@
 class MisteryNumber
   def calculate(operation)
-   
+    value = 0
+    mistery_number = "?"
+
+    loop do
+      expressao = operation.gsub(mistery_number, value.to_s)
+      break if eval(expressao.gsub("=", "=="))
+      value += 1
+    end
+
+    value
   end
 end
+
+# Explicando linha a linha:
+# value = 0: Começa tentando com o número 0.
+# expression.gsub('?', value.to_s): Substitui o ? pela tentativa atual.
+# .gsub('=', '=='): Transforma = em ==, porque o eval precisa de comparação lógica.
+# eval(...): Executa a expressão.
+# Se for verdadeiro, retorna o value.
+# Caso contrário, continua tentando com o próximo valor.
+# .gsub é um método da classe String que significa "global substitution" (substituição global).
+# Ele substitui todas as ocorrências de um padrão por outro dentro de uma string.
