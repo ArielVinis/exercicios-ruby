0c428e9f90c5f377dc1f0bf68202f37766ec8d75
Ariel França <ariel.franca@hotmail.com>
2025-05-16 12:16:09 -0300

    Completed sum_text method to extract and sum integers

--- 7_sum-numbers/lib/numbers.rb
+++ 7_sum-numbers/lib/numbers.rb
@@ -1,5 +1,16 @@
 class Numbers
   def sum_text(numbers_text)
-    
+    numbers_text.scan(/[+-]?\d+/).map(&:to_i).sum
   end
 end
+
+# Poderia usar .lines ou .scan para pegar os números.
+# .lines → pega cada linha do texto e cria um array com elas, serve para quando o texto está mais ordenado.
+# .scan → pega todos os números do texto e cria um array com eles, serve para quando o texto não está tão ordenado.
+# .map(&:to_i) → transforma cada elemento do array em um número inteiro.
+# .sum → soma todos os elementos do array.
+
+# Exemplo:
+# text = "Valores: +7, -3, +10, -0 e outros como 100 e -200."
+# text.scan(/[+-]?\d+/)
+# => ["+7", "-3", "+10", "-0", "100", "-200"]
