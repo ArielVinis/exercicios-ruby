3cd331388d6281641dde887b7e9e41e10baae382
Ariel França <ariel.franca@hotmail.com>
2025-05-16 11:22:48 -0300

    Completed check method to validate Sudoku list completeness and order

--- 6_sudoku/lib/sudoku.rb
+++ 6_sudoku/lib/sudoku.rb
@@ -1,4 +1,10 @@
 class Sudoku
   def check(list)
+    list.sort == (1..9).to_a
   end
 end
+
+# list.sort → ordena a lista
+# (1..9).to_a → cria um array com os números de 1 a 9
+# list.sort == (1..9).to_a → verifica se a lista está ordenada e contém todos os números de 1 a 9
+# O método .to_a no Ruby é usado para converter um objeto em um array, quando esse objeto pode ser representado como uma coleção de elementos.
